a8b6670bf1f520fb32ec9d478715b2215698de17
Sean Stasiak <sstasiak@gmail.com>
2013-03-18 17:21:43 -0400

    mpool_init implemented and tested

--- src/mpool/mpool_init.s
+++ src/mpool/mpool_init.s
@@ -12,41 +12,27 @@
 #         r4 - desired number of blocks (>=2)
 #         r5 - desired sizeof blocks in bytes (>=4)
 #   retval: none
-#   clobbers: nothing more than original args
+#   clobbers: r0,r3,ctr
 # -----------------------------------------------------------------------------
-        .offset
-?rsp:   .long       0
-?lr:    .long       0
-?r30:   .long       0                       #< sizeof blks
-?r31:   .long       0                       #< topof raw blk
-?fs     .equ        .                       #< frame size
-
         .section    .text_vle
-mpool_init:
-        e_stwu      rsp, -?fs(rsp)
-        se_mflr     r0
-        se_stw      r0, ?lr(rsp)
-#       tweqi       r3, 0                   #< block ptr can't be null
-        se_stw      r31, ?r31(rsp)
-        se_mr       r31, r3
-#       twlti       r5, 4                   #< sizeof >= 4
-        se_stw      r30, ?r30(rsp)
-        se_mr       r30, r5
-#       twlti       r4, 2                   #< 2+ blocks p/raw blk
+mpool_init:                                 #  see note 0 about traps
+        se_li       r0, 4                   #< sizeof >= 4
+        twlt        r5, r0
+        se_li       r0, 2                   #< 2+ blocks p/raw blk
+        twlt        r4, r0
+        se_li       r0, 0
+        tweq        r3, r0                  #< raw blk !null
+        se_stw      r0, 0(r3)               #< terminate the list
         se_mtctr    r4
-
-        e_bl        fifo_init               #< r2 validated here
-
-# foreach block
-#   start peeling off blocks and pushing them into the fifo
-
-        se_lwz      r30, ?r30(rsp)
-        se_lwz      r31, ?r31(rsp)
-        se_lwz      r0, ?lr(rsp)
-        se_mtlr     r0
-        e_lwz       rsp, ?rsp(rsp)
+@next:  e_bdz       @done
+        stwux       r3, r3, r5              #< update and link back
+        se_b        @next
+@done:  se_stw      r3, head(r2)            #< store off head
         se_blr
 .function   "mpool_init", mpool_init, .-mpool_init
+# -----------------------------------------------------------------------------
+# note 0: turns out trap word immediate isn't available in VLE. you can
+#         only trap on reg-to-reg comparisons.
 # -----------------------------------------------------------------------------
 # Copyright (c) 2013, Sean Stasiak. All rights reserved.
 # Developed by: Sean Stasiak <sstasiak@gmail.com>
