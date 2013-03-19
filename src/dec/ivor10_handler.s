130035869a57371f5d16de2b981a7d49a796c487
Sean Stasiak <sstasiak@gmail.com>
2013-03-19 16:28:16 -0400

    use more descriptive stack offsets and allow nestable int for lower latency

--- src/dec/ivor10_handler.s
+++ src/dec/ivor10_handler.s
@@ -10,27 +10,48 @@
 #   decrementer interrupt handler:
 #   this interrupts is so short, I don't bother unmasking. if you do more here
 #   consider this as a point of optimization to keep int latency low.
+#   clobbers: r2,r3,r4,lr - but restored before exit since this is an interrupt
 # -----------------------------------------------------------------------------
+        .offset
+?rsp:   .long       0
+?lr:    .long       0
+?r2:    .long       0
+?r3:    .long       0
+?r4:    .long       0
+?srr0:  .long       0
+?srr1:  .long       0
+?fs     .equ        .                       #< frame size
+
         .section    .text_vle
         .align      16
 ivor10_handler:
-        e_stwu      rsp, -16(rsp)           #< create frame
-        se_stw      r2, 12(rsp)             #< push r2
-        se_stw      r3, 8(rsp)              #< push r3
-        se_stw      r4, 4(rsp)              #< push r4
+        e_stwu      rsp, -?fs(rsp)
+        e_stmvsrrw  ?srr0(rsp)
+        se_stw      r2, ?r2(rsp)
         se_bgeni    r2, 4
         mttsr       r2                      #< clr TSR[DIE]
-        e_lwz       r2, msec@l(r24)
-        se_addi     r2, 1                   #< msec++
-        e_stw       r2, msec@l(r24)
+        wrteei      1                       #< unmask
+
+        e_lwz       r2, msec@l(r24)         #< !atomic, but msec is ro
+        se_addi     r2, 1                   #  and therefore safe to modify
+        e_stw       r2, msec@l(r24)         #  in one place only (here)
+
+        se_stw      r3, ?r3(rsp)            #< save remaining context
+        se_stw      r4, ?r4(rsp)
+        se_mflr     r2
+        se_stw      r2, ?lr(rsp)
+
         se_li       r2, LED0
-        se_mflr     r0                      #< safe, no danger of preemption
         se_bl       led_invert
-        se_mtlr     r0
-        se_lwz      r4, 4(rsp)              #< pop r4
-        se_lwz      r3, 8(rsp)              #< pop r3
-        se_lwz      r2, 12(rsp)             #< pop r2
-        se_lwz      rsp, 0(rsp)             #< clear frame
+
+        se_lwz      r2, ?lr(rsp)
+        se_mtlr     r2
+        se_lwz      r4, ?r4(rsp)
+        se_lwz      r3, ?r3(rsp)
+        se_lwz      r2, ?r2(rsp)
+        wrteei      0                       #< mask
+        e_lmvsrrw   ?srr0(rsp)
+        se_lwz      rsp, ?rsp(rsp)
         se_rfi
 .function   "ivor10_handler", ivor10_handler, .-ivor10_handler
 # -----------------------------------------------------------------------------
