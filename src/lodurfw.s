a836080ec1e225946ab95d5e23645ebd5c61c00e
Sean Stasiak <sstasiak@gmail.com>
2013-03-26 22:37:06 -0400

    Realized that code was getting big and taking a long time to download via serial BAM. Going to try something new - poll and no interrupts. Keep it small and simple.

--- src/lodurfw.s
+++ src/lodurfw.s
@@ -1,15 +1,12 @@
 # -----------------------------------------------------------------------------
 # Copyright (c) 2013, Sean Stasiak. All rights reserved.
-# Developed by: Sean Stasiak <sstasiak@gmail.com>
-# Refer to license terms at the bottom of this file
+# Developed by: Sean Stasiak <sstasiak@gmail.com> see 'license.txt' for terms
 # -----------------------------------------------------------------------------
         .include    "led.i"
-        .include    "esci.i"
-        .include    "fifo.i"
-        .include    "lifo.i"
 # -----------------------------------------------------------------------------
 #   @public
-#   lodurfw: app
+#   lodurfw: flash upload app. trying something new here .. no interrupts and
+#            polling everything, not my usual style/preference.
 # -----------------------------------------------------------------------------
         .offset
 ?rsp:   .long       0
@@ -24,11 +21,8 @@ lodurfw:
         se_stw      r0, ?lr(rsp)
 
         e_bl        led_init
-        e_bl        esci_init
-
-@loop:  wait
-        e_bl        lifo_test
-        e_bl        fifo_test
+        se_li       r2, LED0
+@loop:  e_bl        led_invert
         se_b        @loop
 
         se_lwz      r0, ?lr(rsp)
@@ -36,33 +30,4 @@ lodurfw:
         se_lwz      rsp, ?rsp(rsp)
         se_blr
 .function   "lodurfw", lodurfw, .-lodurfw
-# -----------------------------------------------------------------------------
-# Copyright (c) 2013, Sean Stasiak. All rights reserved.
-# Developed by: Sean Stasiak <sstasiak@gmail.com>
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# with the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is furnished
-# to do so, subject to the following conditions:
-#
-#   -Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimers.
-#
-#   -Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimers in the documentation
-#    and/or other materials provided with the distribution.
-#
-#   -Neither Sean Stasiak, nor the names of other contributors may be used to
-#    endorse or promote products derived from this Software without specific
-#    prior written permission.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
-# THE SOFTWARE.
 # -----------------------------------------------------------------------------
